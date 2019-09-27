11ae2fd7d728016a5021f2a05384f852190abba0
gntpldes <41797111+gntpldes@users.noreply.github.com>
2019-09-26 22:02:56 -0500

    Create index.js

---
+++ jQuery_refresher/test_case1/index.js
@@ -0,0 +1,9 @@
+// I've forgotten how much fun jQuery can be
+
+$('h1#heading1').hide(); 
+
+$('ul#list li:first').css('color', '#7d5fff'); 
+$('ul#list li:even').css('color', '#fff200', 'background-color', '#3d3d3d'); 
+$('ul#list li:odd').css('color', '#32ff7e', 'background-color', '#17c0eb'); 
+$('ul#list li:even').css('background-color', '#3d3d3d'); 
+$('ul#list li:odd').css('background-color', '#17c0eb'); 
