f65044cb3df9ac80ba7366288d16324b1e594cfe
gntpldes <41797111+gntpldes@users.noreply.github.com>
2019-10-10 16:25:18 -0500

    Create main.js

---
+++ vue2/main.js
@@ -0,0 +1,10 @@
+var app = new Vue({
+    el: '#app', 
+    data: {
+        product: 'Pans', 
+        description: 'For all your cooking needs',
+        image: '/media/pan.jpg',
+        inventory: 5, 
+        inStock: true
+    }
+})
