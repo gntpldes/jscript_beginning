f876ac5ff8f2ba44524ec84fe52053b0178c77e1
gntpldes <41797111+gntpldes@users.noreply.github.com>
2019-10-22 14:32:11 -0500

    Add files via upload
    React Refresher App Content, I figured it'd be a good idea to upload before I delete the file on my machine.

---
+++ react_recipes/src/Test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Test = () => {
+    return(
+        <div className="testDiv">
+            <h1>IT FUCKING WORKS!</h1>
+        </div>
+    );
+}
+
+export default Test;
